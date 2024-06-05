5bcca6d0658bcd63a854406bf1dd43d168a551fb
Kilhub <killuaaaaaaaa1@gmail.com>
2024-06-06 01:17:32 +0800

    Add token_required decorator for JWT authentication

--- api.py
+++ api.py
@@ -25,3 +25,26 @@ def data_fetch(query, params=None):
     data = cur.fetchall()
     cur.close()
     return data
+
+from functools import wraps
+from flask import request, jsonify
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = data['user']
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+    return decorated
